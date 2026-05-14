4c6fdce640ed159ca16602dafa5e75c9696db89e
Ethan Sheppard <32421150+Ethameshep@users.noreply.github.com>
2026-05-13 19:41:40 -0500

    Update dvh-support-elementor.php

--- dvh-support-elementor.php
+++ dvh-support-elementor.php
@@ -1,73 +1,81 @@
 <?php
 /**
- * Plugin Name: DVH Support for Elementor
- * Description: Automatically adds dvh unit support to Elementor 'vh' controls with a standard 'vh' fallback.
+ * Plugin Name: DVH Min-Height Support for Elementor Containers
+ * Description: Automatically adds dynamic viewport height (dvh) to the 'Min Height' controls of Elementor Containers for perfect mobile responsiveness.
  * Version: 1.0
  * Author: Ethan Sheppard
- * License: GPLv2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: dvh-support-for-elementor
+ * Text Domain: dvh-support-elementor
+ * Requires Plugins: elementor
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-class DVH_Support_For_Elementor {
+if ( ! class_exists( 'DVH_MH_SUPPORT' ) ) {
 
-    public function __construct() {
-        add_action( 'plugins_loaded', [ $this, 'init' ] );
-    }
+    class DVH_MH_SUPPORT {
 
-    public function init() {
-        if ( ! did_action( 'elementor/loaded' ) ) {
-            add_action( 'admin_notices', [ $this, 'admin_notice_missing_elementor' ] );
-            return;
+        const MINIMUM_ELEMENTOR_VERSION = '3.16.0';
+
+        public function __construct() {
+            add_action( 'plugins_loaded', [ $this, 'init' ] );
         }
 
-        add_filter( 'elementor/element/parse_css', [ $this, 'add_dvh_fallback' ], 10, 2 );
-    }
+        public function init() {
+            if ( ! did_action( 'elementor/loaded' ) ) {
+                return;
+            }
 
-    public function add_dvh_fallback( $post_css, $element ) {
-        $settings = $element->get_settings();
+            if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+                return;
+            }
 
-        if ( 
-            ! empty( $settings['min_height']['unit'] ) && 
-            $settings['min_height']['unit'] === 'vh' &&
-            ! empty( $settings['min_height']['size'] )
-        ) {
-            $value = $settings['min_height']['size'];
-            $selector = $post_css->get_element_unique_selector( $element );
-
-            $custom_css = "
-                {$selector} { 
-                    min-height: {$value}vh; 
-                    min-height: {$value}dvh; 
-                }
-            ";
-
-            $post_css->add_custom_css( $custom_css );
+            add_filter( 'elementor/element/parse_css', [ $this, 'dvh_mh_support_add' ], 20, 2 );
         }
 
-        return $post_css;
+        public function dvh_mh_support_add( $post_css, $element ) {
+            if ( ! isset( \Elementor\Plugin::$instance->experiments ) ) {
+                return $post_css;
+            }
+
+            $experiments = \Elementor\Plugin::$instance->experiments;
+            if ( ! $experiments->is_feature_active( 'container' ) ) {
+                return $post_css;
+            }
+
+            $supported_elements = [ 'container', 'grid' ];
+            if ( ! in_array( $element->get_name(), $supported_elements, true ) ) {
+                return $post_css;
+            }
+
+            $settings = $element->get_settings();
+
+            if ( 
+                ! empty( $settings['min_height']['unit'] ) && 
+                $settings['min_height']['unit'] === 'vh' &&
+                ! empty( $settings['min_height']['size'] )
+            ) {
+                $value = $settings['min_height']['size'];
+                $selector = $post_css->get_element_unique_selector( $element );
+
+                $post_css->get_stylesheet()->add_raw_css( "
+                    {$selector} { 
+                        min-height: {$value}dvh;
+                    }
+                " );
+            }
+
+            return $post_css;
+        }
     }
 
-    public function admin_notice_missing_elementor() {
-        $message = sprintf(
-            esc_html__( '%1$sDVH Support for Elementor%2$s requires %1$sElementor%2$s to be installed and activated.', 'dvh-support-for-elementor' ),
-            '<strong>',
-            '</strong>'
-        );
-
-        printf( '<div class="notice notice-warning is-dismissible"><p>%s</p></div>', $message );
-    }
+    new DVH_MH_SUPPORT();
 }
 
-new DVH_Support_For_Elementor();
+register_activation_hook( __FILE__, 'dvh_mh_support_clear_cache' );
+register_deactivation_hook( __FILE__, 'dvh_mh_support_clear_cache' );
 
-register_activation_hook( __FILE__, 'dvh_support_for_elementor_clear_cache' );
-register_deactivation_hook( __FILE__, 'dvh_support_for_elementor_clear_cache' );
-
-function dvh_support_for_elementor_clear_cache() {
-    if ( did_action( 'elementor/loaded' ) ) {
+function dvh_mh_support_clear_cache() {
+    if ( did_action( 'elementor/loaded' ) && class_exists( '\Elementor\Plugin' ) ) {
         \Elementor\Plugin::instance()->files_manager->clear_cache();
     }
 }
